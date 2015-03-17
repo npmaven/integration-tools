d53b2ae94025dc30a447b4e8ac056d4af0f1edcd
Joe Barnes <barnesjd@gmail.com>
2015-03-16 21:21:18 -0500

    Starting the Package java class

---
+++ src/main/java/org/npmaven/it/Package.java
@@ -0,0 +1,14 @@
+package org.npmaven.it;
+
+public class Package {
+    private final String name;
+
+    public Package(String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+}
--- src/test/scala/org/npmaven/it/PackageSpecs.scala
+++ src/test/scala/org/npmaven/it/PackageSpecs.scala
@@ -4,9 +4,11 @@ package it
 import org.specs2.mutable.Specification
 
 object PackageSpecs extends Specification {
-  "Package object" should {
-    "do stuff" in {
-      true should be equalTo(true)
+  "Package class" should {
+    "have a name constructor/getter" in {
+      val pkg = new Package("blah")
+      val name = pkg.getName
+      name must be equalTo("blah")
     }
   }
 }
